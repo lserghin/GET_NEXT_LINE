09be4adb80d4c99c00794c1274f62f45bda43cff
Loukmane Serghini <lserghin@c2r1p1.1337.ma>
2025-03-17 21:32:37 +0000

    ✅

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: lserghin <lserghin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 20:32:05 by lserghin          #+#    #+#             */
-/*   Updated: 2024/11/26 13:46:50 by lserghin         ###   ########.fr       */
+/*   Updated: 2025/03/17 21:27:05 by lserghin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 # define GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <fcntl.h>
 
@@ -22,10 +21,10 @@
 #  define BUFFER_SIZE 42
 # endif
 
-char	*get_next_line(int fd);
 size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(char *s, int c);
+char	*get_next_line(int fd);
 char	*ft_strdup(char *s);
 size_t	ft_strlen(char *s);
 
